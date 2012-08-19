ea6bd9a2acb65645cf955b9d45e08b9039eacb2d
Trevor Morse <trevor.morse@gmail.com>
2012-08-19 14:07:07 -0300

    Refactor to handle multiple selection properly.

--- AlignEquals.py
+++ AlignEquals.py
@@ -1,16 +1,25 @@
+###############################################################################
+# AlignEquals
+#
+# Plugin for Sublime Text 2 to align equals (=) of all lines in a selection.
+#
+# Created by Trevor Morse <trevor.morse@gmail.com>
+###############################################################################
 import sublime, sublime_plugin
 
 class AlignEqualsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        max_equals = 0
+
+        # Determine highest/furtherest = in the selection
         selections = self.view.sel()
         for selection in selections:
+            max_equals = 0
             for line in self.view.substr(selection).split('\n'):
                 if line.find('=') > max_equals:
                     max_equals = line.find('=')
 
-        lines = ''
-        for selection in selections:
+            # Add necessary spaces before all =
+            lines = ''
             for line in self.view.substr(selection).split('\n'):
                 if (line.find('=') < max_equals) and line.find('=') != -1:
                     equal_pos = line.find('=')
@@ -19,5 +28,7 @@ class AlignEqualsCommand(sublime_plugin.TextCommand):
                     line      = beginning + ' '*(max_equals - equal_pos) + end;
                 lines += line + '\n';
             lines = lines[:-1]
+
+            # replace selection with properly spaced
             self.view.replace(edit, selection, lines)
 
