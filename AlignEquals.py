8b38ab5fa02a8850613f5af97ee7169a81d09273
Trevor Morse <trevor.morse@gmail.com>
2012-08-20 10:29:53 -0300

    Helper functions moved outside of class.

--- AlignEquals.py
+++ AlignEquals.py
@@ -7,6 +7,17 @@
 ###############################################################################
 import sublime, sublime_plugin
 
+def get_len_before_equals(line):
+    return len(line[0:line.find('=')].rstrip())
+
+def format_line(line, pre_len, max_len):
+    beginning = line[0:pre_len]
+    equal_pos = line.find('=')
+    end       = line[equal_pos+1:].lstrip() # skip over =
+    line      = beginning + ' '*(max_len - len(beginning)) + ' = ' + end
+
+    return line
+
 class AlignEqualsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
@@ -15,7 +26,7 @@ class AlignEqualsCommand(sublime_plugin.TextCommand):
         for selection in selections:
             max_len = 0
             for line in self.view.substr(selection).split('\n'):
-                pre_len = self.get_len_before_equals(line);
+                pre_len = get_len_before_equals(line);
                 if  pre_len > max_len:
                     max_len = pre_len
 
@@ -24,22 +35,13 @@ class AlignEqualsCommand(sublime_plugin.TextCommand):
             for line in self.view.substr(selection).split('\n'):
 
                 # Make sure there is actually an = in this line
-                pre_len = self.get_len_before_equals(line);
+                pre_len = get_len_before_equals(line);
                 if pre_len != 0:
-                    line = self.format_line(line, pre_len, max_len)
+                    line = format_line(line, pre_len, max_len)
                 lines += line + '\n';
             lines = lines[:-1]
 
             # replace selection with properly spaced
             self.view.replace(edit, selection, lines)
 
-    def get_len_before_equals(self, line):
-        return len(line[0:line.find('=')].rstrip())
 
-    def format_line(self, line, pre_len, max_len):
-        beginning = line[0:pre_len]
-        equal_pos = line.find('=')
-        end       = line[equal_pos+1:].lstrip() # skip over =
-        line      = beginning + ' '*(max_len - len(beginning)) + ' = ' + end
-
-        return line
