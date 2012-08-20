31696be0ad1bc9a20bf488bc937f7aacca51eecc
Trevor Morse <trevor.morse@gmail.com>
2012-08-20 09:49:39 -0300

    Fix issue #1.
    Add appropriate padding around the =. Should be at least one space
    before and after the =.

--- AlignEquals.py
+++ AlignEquals.py
@@ -10,25 +10,36 @@ import sublime, sublime_plugin
 class AlignEqualsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
-        # Determine highest/furtherest = in the selection
+        # Determine highest/furthest = in the selection
         selections = self.view.sel()
         for selection in selections:
-            max_equals = 0
+            max_len = 0
             for line in self.view.substr(selection).split('\n'):
-                if line.find('=') > max_equals:
-                    max_equals = line.find('=')
+                pre_len = self.get_len_before_equals(line);
+                if  pre_len > max_len:
+                    max_len = pre_len
 
-            # Add necessary spaces before all =
+            # Add necessary padding around =
             lines = ''
             for line in self.view.substr(selection).split('\n'):
-                if (line.find('=') < max_equals) and line.find('=') != -1:
-                    equal_pos = line.find('=')
-                    beginning = line[0:equal_pos]
-                    end       = line[equal_pos:]
-                    line      = beginning + ' '*(max_equals - equal_pos) + end;
+
+                # Make sure there is actually an = in this line
+                pre_len = self.get_len_before_equals(line);
+                if pre_len != 0:
+                    line = self.format_line(line, pre_len, max_len)
                 lines += line + '\n';
             lines = lines[:-1]
 
             # replace selection with properly spaced
             self.view.replace(edit, selection, lines)
 
+    def get_len_before_equals(self, line):
+        return len(line[0:line.find('=')].rstrip())
+
+    def format_line(self, line, pre_len, max_len):
+        beginning = line[0:pre_len]
+        equal_pos = line.find('=')
+        end       = line[equal_pos+1:].lstrip() # skip over =
+        line      = beginning + ' '*(max_len - len(beginning)) + ' = ' + end
+
+        return line
